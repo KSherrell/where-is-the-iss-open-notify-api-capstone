cbbd39006edbb9f04563dbc349fd23d727d9a63b
Kimzilla <kim.fsw@gmail.com>
2017-09-03 22:56:54 -0600

    added some responsive styles, created custom maps icon, removed the find ISS buttom and added setInterval to automatically update the map every 5 seconds

--- main-js.js
+++ main-js.js
@@ -1,54 +1,60 @@
 "use strict";
 
+var refreshLoc;
+// alert("ready!");
 
-$(document).ready(function () {
-    /* Update all the parameters for your API test*/
-    /* var params = {
-         tagged: 'html',
-         site: 'stackoverflow',
-         order: 'desc',
-         sort: 'creation'
-     };*/
-    $(".js-currentTime").click(function () {
-        var currentLocation = $.ajax({
-                url: "http://api.open-notify.org/iss-now.json",
-                dataType: "jsonp",
-                type: "GET"
-            })
-            .done(function (currentLocation) {
-                console.log(currentLocation);
+function refreshLocation() {
+    refreshLoc = setInterval(currLoc, 5000);
+    //alert("refresh!");
+}
 
-                var issLat = Number(currentLocation.iss_position.latitude);
-                var issLon = Number(currentLocation.iss_position.longitude);
-                initMap(issLat, issLon);
-                $("#description").text("The International Space Station is currently flying over " + issLat + " latitude, " + issLon + " longitude.")
-            })
+function currLoc() {
+    var currentLocation = $.ajax({
+            url: "http://api.open-notify.org/iss-now.json",
+            dataType: "jsonp",
+            type: "GET"
+        })
+        .done(function (currentLocation) {
+            console.log(currentLocation);
 
-            .fail(function (jqXHR, error, errorThrown) {
-                console.log(jqXHR);
-                console.log(error);
-                console.log(errorThrown);
-            });
-    })
+            var issLat = Number(currentLocation.iss_position.latitude);
+            var issLon = Number(currentLocation.iss_position.longitude);
+            initMap(issLat, issLon);
+            $("#description").text("The International Space Station is currently flying over " + issLat + " latitude, " + issLon + " longitude.")
+        })
 
+        .fail(function (jqXHR, error, errorThrown) {
+            console.log(jqXHR);
+            console.log(error);
+            console.log(errorThrown);
+        });
+}
 
-});
 var map;
 
 function initMap(issLat, issLon) {
     //alert("map!");
-    $("#map")
     var uluru = {
         lat: issLat,
         lng: issLon
     };
     console.log(uluru);
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
+        zoom: 7,
+        center: uluru,
+        disableDefaultUI: true,
+        zoomControl: true
+
     });
+    var iconBase = 'imgs/iss_icon.png';
     var marker = new google.maps.Marker({
         position: uluru,
-        map: map
+        map: map,
+        icon: iconBase
     });
 }
+
+
+
+$(currLoc());
+$(refreshLocation());
